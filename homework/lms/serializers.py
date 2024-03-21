0635351de99583521b8e5b974ef85bd05a0f33fe
PC <kerik006@mail.ru>
2024-03-21 10:49:58 +0300

    Homeworkыы

--- homework/lms/serializers.py
+++ homework/lms/serializers.py
@@ -3,28 +3,6 @@ from rest_framework import serializers
 from .models import Course
 
 
-class CourseSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Course
-        fields = '__all__'
-
-
-class LessonSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Lesson
-        fields = '__all__'
-
-
-class CourseSerializer(serializers.ModelSerializer):
-    lessons_count = serializers.SerializerMethodField()
-
-    class Meta:
-        model = Course
-        fields = ['id', 'title', 'preview', 'description', 'lessons_count']
-
-    def get_lessons_count(self, obj):
-        return obj.lessons.count()
-
 class LessonSerializer(serializers.ModelSerializer):
     class Meta:
         model = Lesson
